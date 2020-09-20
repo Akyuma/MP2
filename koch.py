786b1f52abefded03a2bcbe2e3f57ffb5de75153
Mallory_P <70748355+Akyuma@users.noreply.github.com>
2020-09-20 19:36:31 +0200

    Add files via upload

---
+++ koch.py
@@ -0,0 +1,34 @@
+﻿from turtle import *
+try :
+    reset()
+except Terminator:
+    pass
+def koch(L, n):
+
+    if n == 0:
+        forward(L)
+    else:
+        koch(L/3, n-1)
+        left(60)
+        koch(L/3, n-1)
+        right(120)
+        koch(L/3, n-1)
+        left(60)
+        koch(L/3, n-1)
+
+def flocon(T, etape):
+    koch(T, etape)
+    right(120)
+    koch(T, etape)
+    right(120)
+    koch(T, etape)
+
+
+up()
+goto(-280, 100)
+down()
+speed(50)
+
+flocon(100, 3)
+
+done()
